7c90e296a37dce86ebad60921a546121447661f6
Allaaxx <allanrao358@gmail.com>
2025-10-06 10:50:04 -0300

    fix: ajustar a ordem das classes no componente footer para melhor estruturação do layout

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="mt-auto bg-accent w-full gap-1 p-8">
+    <footer className="bg-accent mt-auto w-full gap-1 p-8">
       <p className="text-xs font-medium">© 2025 Copyright LOKI</p>
       <p className="text-muted-foreground text-xs font-medium">
         Todos os direitos reservados.
