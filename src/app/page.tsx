855d6944c8faa0d053504c7e1df01b047a0f3211
Allaaxx <allanrao358@gmail.com>
2025-10-06 10:49:44 -0300

    fix: ajustar a estrutura dos banners para melhor responsividade e estilo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,12 +27,12 @@ const Home = async () => {
       <Header />
       <div className="container mx-auto space-y-6">
         <div className="px-5">
-          <div className="bg-muted relative aspect-[3/1] w-full overflow-hidden rounded-md md:aspect-[4/1]">
+          <div className=" relative w-full overflow-hidden rounded-md h-[48vw] md:h-[36vw] lg:h-[28vw] xl:h-[60vw]">
             <Image
               src="/banner-01.png"
               alt="Leve uma vida com estilo"
               fill
-              className="object-contain p-4"
+              className="object-contain rounded-md"
             />
           </div>
         </div>
@@ -44,12 +44,12 @@ const Home = async () => {
         </div>
 
         <div className="px-5">
-          <div className="bg-muted relative aspect-[3/1] w-full overflow-hidden rounded-md md:aspect-[4/1]">
+          <div className="relative w-full overflow-hidden rounded-md h-[48vw] md:h-[36vw] lg:h-[28vw] xl:h-[60vw]">
             <Image
               src="/banner-02.png"
               alt="Leve uma vida com estilo"
               fill
-              className="object-contain p-4"
+              className="object-contain"
             />
           </div>
         </div>
